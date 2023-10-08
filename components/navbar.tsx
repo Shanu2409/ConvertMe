7b6b1eb58335cb16e05f0a58a03051c0f904e6f2
shanu2409 <shanusingh2409@gmail>
2023-10-09 00:15:58 +0530

    updated

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -25,62 +25,6 @@ export default function Navbar({}): any {
           width={100}
         />
       </Link>
-      <div className="gap-1 md:gap-2 lg:gap-4 hidden md:flex">
-        <Button variant="ghost" className="font-semibold text-md">
-          <Link href="/">Home</Link>
-        </Button>
-        <Link href="/about">
-          <Button variant="ghost" className="font-semibold text-md">
-            About
-          </Button>
-        </Link>
-        <Link href="/privacy-policy">
-          <Button variant="ghost" className="font-semibold text-md">
-            Privacy Policy
-          </Button>
-        </Link>
-      </div>
-
-      {/* MOBILE NAV */}
-      <Sheet>
-        <SheetTrigger className="block md:hidden p-3">
-          <span className="text-2xl">
-            <GrMenu />
-          </span>
-        </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetDescription>
-              <div className="w-full space-y-3">
-                <Link href="/">
-                  <Button
-                    variant="link"
-                    className="font-semibold text-md w-full"
-                  >
-                    Home
-                  </Button>
-                </Link>
-                <Link href="/about">
-                  <Button
-                    variant="link"
-                    className="font-semibold text-md w-full"
-                  >
-                    About
-                  </Button>
-                </Link>
-                <Link href="/privacy-policy">
-                  <Button
-                    variant="link"
-                    className="font-semibold text-md w-full"
-                  >
-                    Privacy Policy
-                  </Button>
-                </Link>
-              </div>
-            </SheetDescription>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
     </nav>
   );
 }
